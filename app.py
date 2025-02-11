87af2194f049723658b2fbc9a59a5dfc34406261
Ajobiewe Joseph <joseph4hacking@gmail.com>
2025-02-11 03:29:15 +0100

    fixed a bug i discovered with my recent deployment

--- app.py
+++ app.py
@@ -14,20 +14,29 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=['*'],
                    allow_headers=['*'])
 
+@app.get('/')
+async def home():
+    return JSONResponse(status_code=404,
+                        content={'error': True,
+                                 'message': 'go to /api/classify-number?number=<number>'
+                        }
+                        )
+
 @app.get('/api/classify-number')
 async def hello(number):
-    if number != str(int(number)):
-        return JSONResponse(status_code=400,
-                            content={'error': True}
-                            )
     try:
-        number = int(number)
+        type_casted_number = int(number)
     except:
         return JSONResponse(status_code=400,
                            content={'number':'alphabet',
                                     'error': True
                             }
         )
+    if number != str(type_casted_number):
+        return JSONResponse(status_code=400,
+                            content={'error': True}
+                            )
+    number = type_casted_number
     content = requests.get(f'http://numbersapi.com/{number}/math?json')
     result = {'number': number,
               'is_prime': is_prime(number),
